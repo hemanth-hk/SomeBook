aefb6e0a24a0a64615a92cbc59264aa1482908d5
hemanth-hk <uppadahemanthkumar27@gmail.com>
2020-03-16 16:53:00 +0530

    Added home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MainGrid msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainGrid from '@/components/Maingrid.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MainGrid
   }
 }
 </script>
